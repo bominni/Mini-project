b1bdbe071465f1b47bbe206dbc07c8f0b71725c7
bominni <bom.iiini@gmail.com>
2023-12-07 12:12:51 +0900

    feat: 게시판 수정(#6)

--- mini/routes/posts.py
+++ mini/routes/posts.py
@@ -12,19 +12,29 @@ async def retrieve_post() -> List[Post]:
 
 @post_router.post("/create")
 async def create_post(body: Post = Body(...)) -> dict:
-    posts.append(body)
+    posts.append(dict(body))
     return {
         "message": "게시물이 생성되었습니다."
     }
 
-@post_router.patch("/change/{id}")
-async def change_post(id: int) -> dict:
-    print("")
+@post_router.patch("/change")
+async def change_post(body: Post = Body(...)) -> dict:
+    for post in posts:
+        if body["id"] == post["id"]:
+            post["title"] = body["title"]
+            post["content"] = body["content"]
+            return {
+                "message" : "수정되었습니다."
+            }
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="해당 게시판은 없는 게시판입니다. 다시 한번 확인해주세요."
+    )
 
 @post_router.delete("/delete/{id}")
 async def delete_post(id: int) -> dict:
     for post in posts:
-        if post in posts:
+        if id == post["id"]:
             posts.remove(post)
             return {
                 "message": "해당 글이 삭제되었습니다."
